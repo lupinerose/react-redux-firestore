4488a39e66798a2c375491175c9caa703ea771c6
Brooke Kullberg <brookekullberg@gmail.com>
2020-05-04 10:27:21 -0700

    remove root reducer tests for masterTicketList

--- src/__tests__/reducers/index-reducer.test.js
+++ src/__tests__/reducers/index-reducer.test.js
@@ -1,7 +1,6 @@
 import rootReducer from '../../reducers/index';
 import { createStore } from 'redux';
 import formVisibleReducer from '../../reducers/form-visible-reducer';
-import ticketListReducer from '../../reducers/ticket-list-reducer';
 import * as c from './../../actions/ActionTypes';
 
 let store = createStore(rootReducer);
@@ -14,25 +13,9 @@ describe("rootReducer", () => {
   //     formVisibleOnPage: false
   //   });
   // });
-
-  test('Check that initial state of ticketListReducer matches root reducer', () => {
-    expect(store.getState().masterTicketList).toEqual(ticketListReducer(undefined, { type: null }));
-  });
   
   test('Check that initial states of formVisibleReducer matches root reducer', () => {
     expect(store.getState().formVisibleOnPage).toEqual(formVisibleReducer(undefined, { type: null }));
-  });
-
-  test('Check that initial state of ticketListReducer matches root reducer', () => {
-    const action = {
-      type: c.ADD_TICKET,
-      names: 'Ryan & Aimen',
-      location: '4b',
-      issue: 'Redux action is not working correctly.',
-      id: 1
-    }
-    store.dispatch(action);
-    expect(store.getState().masterTicketList).toEqual(ticketListReducer(undefined, action));
   });
   
   test('Check that initial state of formVisibleReducer matches root reducer', () => {
