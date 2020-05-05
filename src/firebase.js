c50f5c4f6730e718940a0b8f0414edcf0aa4271c
Brooke Kullberg <brookekullberg@gmail.com>
2020-05-04 22:20:37 -0700

    add react router to project

--- src/firebase.js
+++ src/firebase.js
@@ -11,6 +11,16 @@ const firebaseConfig = {
   appId: process.env.REACT_APP_FIREBASE_APP_ID 
 }
 
+// const firebaseConfig = {
+//   apiKey: `${process.env.REACT_APP_FIREBASE_API_KEY}`,
+//   authDomain: `${process.env.REACT_APP_FIREBASE_AUTH_DOMAIN}`,
+//   databaseURL: `${process.env.REACT_APP_FIREBASE_DATABASE_URL}`,
+//   projectId: `${process.env.REACT_APP_FIREBASE_PROJECT_ID}`,
+//   storageBucket: `${process.env.REACT_APP_FIREBASE_STORAGE_BUCKET}`,
+//   messagingSenderId: `${process.env.REACT_APP_FIREBASE_SENDER_ID}`,
+//   appId: `${process.env.REACT_APP_FIREBASE_APP_ID}`
+// }
+
 firebase.initializeApp(firebaseConfig);
 firebase.firestore();
 
@@ -23,12 +33,3 @@ export default firebase;
 
 
 
-// const firebaseConfig = {
-//   apiKey: `${process.env.REACT_APP_FIREBASE_API_KEY}`,
-//   authDomain: `${process.env.REACT_APP_FIREBASE_AUTH_DOMAIN}`,
-//   databaseURL: `${process.env.REACT_APP_FIREBASE_DATABASE_URL}`,
-//   projectId: `${process.env.REACT_APP_FIREBASE_PROJECT_ID}`,
-//   storageBucket: `${process.env.REACT_APP_FIREBASE_STORAGE_BUCKET}`,
-//   messagingSenderId: `${process.env.REACT_APP_FIREBASE_SENDER_ID}`,
-//   appId: `${process.env.REACT_APP_FIREBASE_APP_ID}`
-// }
