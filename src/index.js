7b1ab7f7f8fd7f60e6ac75ba24fced6bfb2832ca
Brooke Kullberg <brookekullberg@gmail.com>
2020-05-04 22:40:14 -0700

    add authentication to project

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ import { Provider } from 'react-redux';
 import { ReactReduxFirebaseProvider } from 'react-redux-firebase';
 import { createFirestoreInstance } from 'redux-firestore';
 import firebase from "./firebase";
+import 'firebase/auth';
 
 
 const store = createStore(
@@ -21,7 +22,8 @@ const store = createStore(
 const rrfProps = {
   firebase,
   config: {
-        userProfile: "users"
+        userProfile: "users",
+        useFirestoreForProfile: true
     },
   dispatch: store.dispatch,
   createFirestoreInstance
